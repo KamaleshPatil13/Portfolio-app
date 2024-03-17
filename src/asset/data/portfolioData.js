6753b9e20c5004e5b08bd9eaad88bf5ce9b2853d
13kamaleshpatil <13kamaleshpatil@gmail.com>
2024-03-17 23:47:36 +0530

    Updated:- - Deploy Script - PortfolioData

--- src/asset/data/portfolioData.js
+++ src/asset/data/portfolioData.js
@@ -8,7 +8,19 @@ const portfolio = [
     title: "Expense Tracker Website",
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-    technologies: ["React js", "CSS"],
-    siteURL: "#",
+    technologies: ["React js", "Context API"],
+    siteURL: "https://kamaleshpatil13.github.io/Expense-Tracker/",
+  },
+  {
+    id: "02",
+    imageURL: expenseTrackerImg1,
+    category: "Portfolio App",
+    title: "Portfolio App Website",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    technologies: ["React js", "Tailwind css"],
+    siteURL: "https://kamaleshpatil13.github.io/Portfolio-app/",
   },
 ];
+
+export default portfolio;
